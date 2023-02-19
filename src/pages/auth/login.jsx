9f0119ab77b35c8d90f45ce3a8410ddc1da01cf9
Sachin Sharma <sachinkrsharmaa@gmail.com>
2023-02-19 11:53:18 +0530

    connect login and signup page with the api

--- src/pages/auth/login.jsx
+++ src/pages/auth/login.jsx
@@ -1,8 +1,45 @@
 import Head from "next/head";
 import Link from "next/link";
 
+import { useState } from "react";
+import { useRouter } from "next/router";
+
 
 const Login = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+    const router = useRouter();
+  
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+      setIsLoading(true);
+      setError(null);
+  
+      try {
+        const response = await fetch("https://api.stipill.in/auth/login", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ email, password }),
+        });
+  
+        if (response.ok) {
+          router.push("/feeds"); // redirect to dashboard page on success
+        } else {
+          const data = await response.json();
+          setError(data.message);
+        }
+      } catch (error) {
+        console.error(error);
+        setError("An unexpected error occurred. Please try again later.");
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
     return (  
         <div className="col-md-8 col-11 m-auto py-5 ">
             <Head>
@@ -12,18 +49,34 @@ const Login = () => {
             
             <h2 className="heading text-center mb-5">Welcome Back!</h2>
 
-            <form action="#" className="form-groups mt-5">
+            <form onSubmit={handleSubmit} className="form-groups mt-5">
+                {error && <p style={{ color: "red" }}>{error}</p>}
+
                 <div className="col-md-6 col-11 m-auto">
-                    <label htmlFor="username" className="text-sm text-muted mb-1">Username *</label>
-                    <input type="text" name="username" id="username" className="form-control mb-3" />
+                    <label htmlFor="username" className="text-sm text-muted mb-1">Email *</label>
+                    <input
+                        type="email"
+                        id="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        className="form-control mb-3" 
+                    />
                 </div>
                
                 <div className="col-md-6 col-11 m-auto">
                     <label htmlFor="password" className="text-sm text-muted mb-1">Password *</label>
-                    <input type="password" name="password" id="password" className="form-control mb-3" />
+                    <input
+                        type="password"
+                        id="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        className="form-control mb-3" 
+                    />
                 </div>
                 <div className="col-md-6 col-11 m-auto">
-                    <input type="submit" value="Login" className="form-control btn button my-4" />
+                    <button type="submit" disabled={isLoading} className="form-control btn button my-4" >
+                        {isLoading ? "Logging in..." : "Log in"}
+                    </button>
                 </div>
             </form>   
 
--- src/pages/auth/signup.jsx
+++ src/pages/auth/signup.jsx
@@ -1,31 +1,49 @@
 import Head from "next/head";
 import Link from "next/link";
+import axios from 'axios';
 
 import { useState } from "react";
+import { useRouter } from 'next/router'
 
 
 
 const SignUp = () => {
+    const router = useRouter()
 
-    const [username, setUsername] = useState("");
+    const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+  
     const handleSubmit = async (e) => {
-        e.preventDefault();
-
-        const response = await fetch("https://63efc2a8c59531ccf1766922.mockapi.io/auth/users", {
-            method: "POST",
-            body: JSON.stringify({ username, email, password }),
-        })
-
+      e.preventDefault();
+      setIsLoading(true);
+      setError(null);
+  
+      try {
+        const response = await fetch("https://api.stipill.in/auth/signup", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ name, email, password }),
+        });
+  
         if (response.ok) {
-        // redirect to success page or login page
+          router.push("/auth/login");
         } else {
-        // handle error response
+          const data = await response.json();
+          setError(data.message);
         }
-    }
-
+      } catch (error) {
+        console.error(error);
+        setError("An unexpected error occurred. Please try again later.");
+      } finally {
+        setIsLoading(false);
+      }
+    };
+  
 
     return (  
         <div className="col-md-8 col-11 m-auto py-5 ">
@@ -37,21 +55,45 @@ const SignUp = () => {
             <h2 className="heading text-center mb-5">Create an Account</h2>
 
             <form onSubmit={handleSubmit} className="form-groups mt-5">
+
+                {error && <p style={{ color: "red" }}>{error}</p>}
+
                 <div className="col-md-6 col-11 m-auto">
-                    <label htmlFor="username" className="text-sm text-muted mb-1">Username *</label>
-                    <input type="text" name="username" id="username" value={username} className="form-control mb-3" onChange={(e) => setUsername(e.target.value)}/>
+                    <label htmlFor="name" className="text-sm text-muted mb-1">Name *</label>
+                    <input
+                        type="text"
+                        id="name"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                        className="form-control mb-3" 
+                    />
                 </div>
                 <div className="col-md-6 col-11 m-auto">
                     <label htmlFor="email" className="text-sm text-muted mb-1">Email *</label>
-                    <input type="email" name="email" id="email" className="form-control mb-3" onChange={(e) => setEmail(e.target.value)} />
+                    <input
+                        type="email"
+                        id="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        className="form-control mb-3" 
+                    />
                 </div>
                 <div className="col-md-6 col-11 m-auto">
                     <label htmlFor="password" className="text-sm text-muted mb-1">Password *</label>
-                    <input type="password" name="password" id="password" className="form-control mb-3" onChange={(e) => setPassword(e.target.value)} />
+                    <input
+                        type="password"
+                        id="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        className="form-control mb-3" 
+                    />
                 </div>
                 <div className="col-md-6 col-11 m-auto">
-                    <input type="submit" value="Sign Up &rarr;" className="form-control btn button my-4" />
+                    <button type="submit" disabled={isLoading}  className="form-control btn button my-4" >
+                        {isLoading ? "Signing up..." : "Sign Up"}
+                    </button>
                 </div>
+                
             </form>
 
             <div class="text-center">
